6a7715caddcb993c6dc865b1c007646c8a3a8e61
qiannanling <lingqiannan@gmail.com>
2016-04-12 09:59:05 -0400

    simple wall strategy by Hanyu

--- myplayer.cpp
+++ myplayer.cpp
@@ -1,11 +1,12 @@
 #include "myplayer.h"
+#include <iostream>
 #include <QDebug>
-
-bool caught1 = false;
-bool caught2 = false;
-bool caught3 = false;
-bool caught4 = false;
-
+#include <math.h>
+double xFrog = 0;
+double yFrog = 0;
+glm::vec2 frog;
+int step = 0;
+int stepfor2 = 0;
 MyPlayer::MyPlayer()
 {
     this->playerName = "My Player";
@@ -24,7 +25,22 @@ glm::vec2 MyPlayer::initializeFrog(QVector<QVector<int> >* board) {
      * This places the frog in the center.
      * But you can place it anywhere you like!
      */
-    return glm::vec2(250.0f, 250.0f);
+    double xret = 250;
+    double yret = 250;
+//    for(int x = 0; x < 500; x++){
+//        for(int y = 0; y < 500; y++){
+//            xret += (*board)[x][y]*x;
+//            yret += (*board)[x][y]*y;
+//        }
+//    }
+//    xret = xret/500;
+//    yret = yret/500;
+
+    xFrog = xret;
+    yFrog = yret;
+    glm::vec2 ret(xFrog,yFrog);
+    frog = ret;
+    return ret;
 }
 
 
@@ -36,7 +52,8 @@ glm::vec2 MyPlayer::initializeFrog(QVector<QVector<int> >* board) {
  * You can access the walls through this object's "walls" field, which is a vector of Wall*
  */
 void MyPlayer::initializeLights(QVector<QVector<int> >* board) {
-
+    step = 0;
+    stepfor2 = 0;
     /*
     // this prints out debugging information about the walls
     qDebug() << "initializeLights: number of walls " << this->walls.size();
@@ -61,21 +78,77 @@ void MyPlayer::initializeLights(QVector<QVector<int> >* board) {
     /*
      * You can, of course, put the lights anywhere you like!
      */
-    this->lights.at(0)->setInitialPosition(69.0f, 69.0f);
+    double xSet = 70.7;
+    double ySet = 70.7;
+//    int count = 0;
+//    for (int i = 0; i < 250 ; i++) {
+//        for (int j = 0; j < 250; j++) {
+//            xSet += (*board)[i][j]*i;
+//            ySet += (*board)[i][j]*j;
+//            count += (*board)[i][j];
+//        }
+//    }
+//    xSet /= count;
+//    ySet /= count;
+    this->lights.at(0)->setInitialPosition(xSet, ySet);
     this->lights.at(0)->trailColor = QColor(255, 255, 255);
 
-    this->lights.at(1)->setInitialPosition(431.0f, 69.0f);
+    xSet = 429.3;
+    ySet = 70.7;
+//    count = 0;
+//    for (int i = 250; i < 500 ; i++) {
+//        for (int j = 0; j < 250; j++) {
+//            xSet += (*board)[i][j]*i;
+//            ySet += (*board)[i][j]*j;
+//            count += (*board)[i][j];
+//        }
+//    }
+//    xSet /= count;
+//    ySet /= count;
+    this->lights.at(1)->setInitialPosition(xSet, ySet);
     this->lights.at(1)->trailColor = QColor(0, 255, 255);
 
-    this->lights.at(2)->setInitialPosition(69.0f, 431.0f);
+    xSet = 70.7;
+    ySet = 429.3;
+//    count = 0;
+//    for (int i = 0; i < 250 ; i++) {
+//        for (int j = 250; j < 500; j++) {
+//            xSet += (*board)[i][j]*i;
+//            ySet += (*board)[i][j]*j;
+//            count += (*board)[i][j];
+//        }
+//    }
+//    xSet /= count;
+//    ySet /= count;
+    this->lights.at(2)->setInitialPosition(xSet, ySet);
     this->lights.at(2)->trailColor = QColor(255, 255, 0);
 
-    this->lights.at(3)->setInitialPosition(431.0f, 431.0f);
+    xSet = 429.3;
+    ySet = 429.3;
+//    count = 0;
+//    for (int i = 250; i < 500 ; i++) {
+//        for (int j = 250; j < 500; j++) {
+//            xSet += (*board)[i][j]*i;
+//            ySet += (*board)[i][j]*j;
+//            count += (*board)[i][j];
+//        }
+//    }
+//    xSet /= count;
+//    ySet /= count;
+    this->lights.at(3)->setInitialPosition(xSet, ySet);
     this->lights.at(3)->trailColor = QColor(255, 0, 255);
+
 
 }
 
-
+//double[] MyPlayer::calcRange(int index) {
+//    double range[4];
+//    range[0] = (index % 2 == 0) ?  0 : 250;
+//    range[1] = (index % 2 == 0) ? 250 : 500;
+//    range[2] = (index > 1) ? 250 : 0;
+//    range[3] = (index > 1) ? 500 : 250;
+//    return ret;
+//}
 
 /*
  * This method is called once per "step" in the simulation.
@@ -87,74 +160,47 @@ void MyPlayer::initializeLights(QVector<QVector<int> >* board) {
 
 void MyPlayer::updateLights(QVector<QVector<int> >* board) {
 
-
-    for (int j = 0; j < this->lights.length(); j++) {
-
-        // this gets the current position of the light
-        glm::vec2 currPos = this->lights.at(j)->getPosition();
-
-        /*
-         * This is a pretty bad solution!
-         * For part 1, modify this code so that the lights bring all the mosquitoes
-         * to the frog within 5000 steps.
-         */
-        if (j == 0) {
-            if (!caught1 && currPos.y < 300.0 && currPos.x >= 69.0) {
-                this->lights.at(j)->moveTo(69.0, currPos.y + 0.577);
-            } else if (currPos.y >= 300.0 && currPos.x < 250.0) {
-                this->lights.at(j)->moveTo(currPos.x + 0.577, 300.0);
-                caught1 = true;
-            } else if (caught1 && currPos.y >= 250.0 && currPos.x >= 250.0) {
-                this->lights.at(j)->moveTo(250.0, currPos.y - 0.577);
-            } else if (caught1 && currPos.y <= 250.0 && currPos.x == 250.0) {
-                this->lights.at(j)->moveTo(250.0, 250.0);
-                caught1 = false;
+    int index = 0;
+    // i = 0 ,1, 2
+    if(step == 0) {
+        for (int i = 0; i < this->lights.length() && i != 3; i++) {
+            if(i==2) index = 3;
+            else{
+                index = i;
             }
+            glm::vec2 currPos = this->lights.at(i)->getPosition();
+            if (i == 0 && currPos.x > 330) step = 1;
+            this->lights[i]->moveTo(currPos.x+0.6*cos(0.5*M_PI*index),currPos.y+0.6*sin(0.5*M_PI*index));
         }
-        else if (j == 1) {
-            if (!caught2 && currPos.y <= 69.0 && currPos.x > 200) {
-                this->lights.at(j)->moveTo(currPos.x - 0.577, 69.0);
-            } else if (currPos.y < 250.0 && currPos.x <= 200.0) {
-                this->lights.at(j)->moveTo(200.0, currPos.y + 0.577);
-                caught2 = true;
-            } else if (caught2 && currPos.y >= 250.0 && currPos.x <= 250.0) {
-                this->lights.at(j)->moveTo(currPos.x + 0.577, 250.0);
-            } else if (caught2 && currPos.y == 250.0 && currPos.x >= 250.0) {
-                this->lights.at(j)->moveTo(250.0, 250.0);
-                caught2 = false;
-            }
+    } else {
+        for (int i = 0; i < this->lights.length() && i != 3; i++) {
+            glm::vec2 currPos = this->lights[i]->getPosition();
+            float length = glm::length(frog - currPos);
+            glm::vec2 deltaVec = (frog - currPos)/length;
+            this->lights[i]->moveTo(currPos.x+0.6*deltaVec.x,currPos.y+0.6*deltaVec.y);
         }
-        else if (j == 2) {
-            if (!caught3 && currPos.y >= 431.0 && currPos.x < 300) {
-                this->lights.at(j)->moveTo(currPos.x + 0.577, 431.0);
-            } else if (currPos.y > 250.0 && currPos.x >= 300.0) {
-                this->lights.at(j)->moveTo(300.0, currPos.y - 0.577);
-                caught3 = true;
-            } else if (caught3 && currPos.y <= 250.0 && currPos.x >= 250.0) {
-                this->lights.at(j)->moveTo(currPos.x - 0.577, 250.0);
-            } else if (caught3 && currPos.y == 250.0 && currPos.x <= 250.0) {
-                this->lights.at(j)->moveTo(250.0, 250.0);
-                caught3 = false;
-            }
-        }
-        else if (j == 3) {
-            if (!caught4 && currPos.y > 200.0 && currPos.x >= 431.0) {
-                this->lights.at(j)->moveTo(431.0, currPos.y - 0.577);
-            } else if (currPos.y <= 200.0 && currPos.x > 250.0) {
-                this->lights.at(j)->moveTo(currPos.x - 0.577, 200.0);
-                caught4 = true;
-            } else if (caught4 && currPos.y <= 250.0 && currPos.x <= 250.0) {
-                this->lights.at(j)->moveTo(250.0, currPos.y + 0.577);
-            } else if (caught4 && currPos.y >= 250.0 && currPos.x == 250.0) {
-                this->lights.at(j)->moveTo(250.0, 250.0);
-                caught4 = false;
-            }
-        }
+    }
 
+    //i == 2 strategy this->light.at(3)
+    if(stepfor2 == 0) {
+        glm::vec2 currPos1 = this->lights.at(3)->getPosition();
+        if (currPos1.x < 50) stepfor2 = 1;
+        this->lights[3]->moveTo(currPos1.x+0.6*cos(0.5*M_PI*2),currPos1.y+0.6*sin(0.5*M_PI*2));
+    } else if (stepfor2 == 1) {
+        glm::vec2 currPos1 = this->lights.at(3)->getPosition();
+        if (currPos1.y < 200) stepfor2 = 3;
+        this->lights[3]->moveTo(currPos1.x+0.6*cos(0.5*M_PI*3),currPos1.y+0.6*sin(0.5*M_PI*3));
+    } else {
+        glm::vec2 currPos1 = this->lights[3]->getPosition();
+        float length = glm::length(frog - currPos1);
+        glm::vec2 deltaVec = (frog - currPos1)/length;
+        this->lights[3]->moveTo(currPos1.x+0.6*deltaVec.x,currPos1.y+0.6*deltaVec.y);
 
     }
 
-
-
 }
+
+
+
+
 
